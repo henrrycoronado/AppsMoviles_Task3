fd4bbc706f1355a465bae1adfb31e91e765f7f52
henrrycoronado <coronadovillcahenrryalberto@gmail.com>
2025-09-05 23:05:05 -0400

    Commit 3

--- Task3.kt
+++ Task3.kt
@@ -3,9 +3,24 @@
  * play.kotlinlang.org
  */
 fun main() {
+    //commit 1
     println("Hello, world!!!")
-    var NombreEstudiante: String = "Henrry Coronado"
-    var NombreMateria: String = "Aplicaciones Moviles"
-    println("Hola y bienvenido $NombreEstudiante")
-    println("Materia: $NombreMateria")
+
+    //commit 2
+    val nombreEstudiante: String = "Henrry Coronado"
+    val nombreMateria: String = "Aplicaciones Moviles"
+    println("Hola y bienvenido $nombreEstudiante")
+    println("Materia: $nombreMateria")
+
+    //commit 3
+    val peso: Double = 59.5
+    val estatura: Double = 1.69
+    val edad: Int = 20
+    var edadPar: Boolean = (edad % 2 == 0)
+    val genero: List<String> = listOf("Masculino", "Femenino")
+    var generoSeleccionado: String = genero[0] 
+    println("Peso (kg): $peso")
+    println("Estatura (m): $estatura")
+    println("Edad: $edad - Es par: $edadPar")
+    println("Genero: $generoSeleccionado") 
 }
