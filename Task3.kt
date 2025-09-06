02f81b0ec8cfe7ec43b2b9e9500eaf60ff7d2e5f
henrrycoronado <coronadovillcahenrryalberto@gmail.com>
2025-09-05 23:25:21 -0400

    Rama2-Commit4

--- Task3.kt
+++ Task3.kt
@@ -1,26 +1,38 @@
-/**
- * You can edit, run, and share this code.
- * play.kotlinlang.org
- */
+val nombreMateria: String = "Aplicaciones Moviles"
+
+fun Saludar(nombre: String, saludo: String) {
+    val saludoValidado: String = if (saludo.isNotEmpty()) saludo else "Hola"
+    val nombreValidado: String = if (nombre.isNotEmpty()) nombre else "Invitado"
+    println("$saludoValidado, $nombreValidado")
+    println("Materia: $nombreMateria")
+}
+
+fun Genero(genero: Int){
+    print("Mi genero es: ")
+    when (genero) {
+        1 -> println("Masculino")
+        2 -> println("Femenino")
+        else -> println("Otro genero")
+    }
+}
+
 fun main() {
-    //commit 1
+    // commit 1
     println("Hello, world!!!")
 
-    //commit 2
-    val nombreEstudiante: String = "Henrry Coronado"
-    val nombreMateria: String = "Aplicaciones Moviles"
-    println("Hola y bienvenido $nombreEstudiante")
-    println("Materia: $nombreMateria")
-
-    //commit 3
+    // commit 2 - 4(mejorado)
+    Saludar("Henrry", "Bienvenido")
+    
+    // commit 3 - 4 (editado)
     val peso: Double = 59.5
     val estatura: Double = 1.69
     val edad: Int = 20
     var edadPar: Boolean = (edad % 2 == 0)
-    val genero: List<String> = listOf("Masculino", "Femenino")
-    var generoSeleccionado: String = genero[0] 
     println("Peso (kg): $peso")
     println("Estatura (m): $estatura")
     println("Edad: $edad - Es par: $edadPar")
-    println("Genero: $generoSeleccionado") 
+
+    // commit 4
+    val genero: Int = 1
+    Genero(genero)
 }
