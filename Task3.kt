3f2dd2eb9cf5e4d976253d283d70616978ec99d9
henrrycoronado <coronadovillcahenrryalberto@gmail.com>
2025-09-10 15:41:42 -0400

    add class student

--- Task3.kt
+++ Task3.kt
@@ -2,25 +2,39 @@
  * You can edit, run, and share this code.
  * play.kotlinlang.org
  */
+
+class Student(
+    val nombreEstudiante: String,
+    val nombreMateria: String,
+    val peso: Double,
+    val estatura: Double,
+    val edad: Int,
+    val genero: String
+) {
+    fun showSaludo(){
+        println("Hola y bienvenido $nombreEstudiante")
+        println("Materia: $nombreMateria")
+    }
+    fun showStudentInfo() {
+        println("Peso (kg): $peso")
+        println("Estatura (m): $estatura")
+        println("Edad: $edad - Es par: ${edad % 2 == 0}")
+        println("Genero: $genero")
+    }
+}
+
 fun main() {
     //commit 1
     println("Hello, world!!!")
 
-    //commit 2
-    val nombreEstudiante: String = "Henrry Coronado"
-    val nombreMateria: String = "Aplicaciones Moviles"
-    println("Hola y bienvenido $nombreEstudiante")
-    println("Materia: $nombreMateria")
-
-    //commit 3
-    val peso: Double = 59.5
-    val estatura: Double = 1.69
-    val edad: Int = 20
-    var edadPar: Boolean = (edad % 2 == 0)
-    val genero: List<String> = listOf("Masculino", "Femenino")
-    var generoSeleccionado: String = genero[0] 
-    println("Peso (kg): $peso")
-    println("Estatura (m): $estatura")
-    println("Edad: $edad - Es par: $edadPar")
-    println("Genero: $generoSeleccionado") 
+    val henrysInfo = Student(
+        nombreEstudiante = "Henrry Coronado",
+        nombreMateria = "Aplicaciones Moviles",
+        peso = 59.5,
+        estatura = 1.69,
+        edad = 20,
+        genero = "Masculino"
+    )
+    henrysInfo.showSaludo()
+    henrysInfo.showStudentInfo()
 }
